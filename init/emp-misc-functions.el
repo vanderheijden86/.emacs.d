c20c1435d1bf3f87642de1de35ccf2091a02655d
André van der Heijden <vanderheijden86@gmail.com>
2017-04-21 14:23:35 +0200

    Turned off tabs --> whitespace auto save hook because of golang.

--- init/emp-misc-functions.el
+++ init/emp-misc-functions.el
@@ -95,18 +95,18 @@ With argument ARG, do this that many times."
                   (set-window-start w2 s1)
                   (setq i (1+ i))))))))
 
-(defun cleanup-buffer-safe ()
-  "Perform a bunch of safe operations on the whitespace content of a buffer.
-Does not indent buffer, because it is used for a before-save-hook, and that
-might be bad."
-  (interactive)
-  (when (not (string-match-p (regexp-quote "akefile") (buffer-file-name)))
-    (untabify (point-min) (point-max))
-    (delete-trailing-whitespace)
-    (set-buffer-file-coding-system 'utf-8)))
+;; (defun cleanup-buffer-safe ()
+;;   "Perform a bunch of safe operations on the whitespace content of a buffer.
+;; Does not indent buffer, because it is used for a before-save-hook, and that
+;; might be bad."
+;;   (interactive)
+;;   (when (not (string-match-p (regexp-quote "akefile") (buffer-file-name)))
+;;     (untabify (point-min) (point-max))
+;;     (delete-trailing-whitespace)
+;;     (set-buffer-file-coding-system 'utf-8)))
 
-;; Various superfluous white-space. Just say no.
-(add-hook 'before-save-hook 'cleanup-buffer-safe)
+;; ;; Various superfluous white-space. Just say no.
+;; (add-hook 'before-save-hook 'cleanup-buffer-safe)
 
 (defun s-trim-left (s)
   "Remove whitespace at the beginning of S."
